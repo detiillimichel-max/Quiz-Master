33159a10c228d5b8ba80e2c5a3e80006a0c32f32
Michel Detiilli <detiillimichel@gmail.com>
2026-05-23 15:15:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,16 +3,14 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS).catch(() => {
-        // ignora erro se ícones não existirem ainda
-      });
+      return cache.addAll(ASSETS);
     })
   );
   self.skipWaiting();
